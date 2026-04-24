c3243567580a1e6125c33c1e4f08d71b0621b976
SarveshTikekar <sarvesh1.tikekar@gmail.com>
2026-04-24 14:12:04 +0530

    optimised for Vercel deployment

--- synthea-frontend/vite.config.js
+++ synthea-frontend/vite.config.js
@@ -12,5 +12,14 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src")
     }
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:3001',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
   }
 })
